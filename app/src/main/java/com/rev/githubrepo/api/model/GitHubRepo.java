800555a78de911f2c0cc3c2beb2cde8630b33d45
ishtian revee <ishtian11@gmail.com>
2018-08-14 12:02:26 +0600

    GitHubRepos basic model created, GitHubClient done, initializing retrofit stuffs

--- app/src/main/java/com/rev/githubrepo/api/model/GitHubRepo.java
+++ app/src/main/java/com/rev/githubrepo/api/model/GitHubRepo.java
@@ -2,4 +2,9 @@ package com.rev.githubrepo.api.model;
 
 public class GitHubRepo {
 
+    // fields
+    private String name;
+
+    // getters
+    public String getName(){ return this.name; }
 }
--- app/src/main/java/com/rev/githubrepo/api/service/GitHubClient.java
+++ app/src/main/java/com/rev/githubrepo/api/service/GitHubClient.java
@@ -1,5 +1,15 @@
 package com.rev.githubrepo.api.service;
 
+import com.rev.githubrepo.api.model.GitHubRepo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface GitHubClient {
 
+    @GET("/users/{user}/repos")
+    Call<List<GitHubRepo>> reposForUser(@Path("user") String user);
 }
