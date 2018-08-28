646cb375944ce1c522646d2b79037a90de23cb39
ishtian revee <ishtian11@gmail.com>
2018-08-28 23:04:53 +0600

    downloading files from server partially implemented [still not getting the file]

--- app/src/main/java/com/rev/githubrepo/ui/activities/DownloadActivity.java
+++ app/src/main/java/com/rev/githubrepo/ui/activities/DownloadActivity.java
@@ -2,14 +2,22 @@ package com.rev.githubrepo.ui.activities;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.rev.githubrepo.R;
 import com.rev.githubrepo.api.service.FileDownloadClient;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import butterknife.OnClick;
 import okhttp3.ResponseBody;
@@ -38,11 +46,14 @@ public class DownloadActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(DownloadActivity.this, new String[]{
                     Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST);
         }
+
+        downloadFile();
     }
 
     @OnClick(R.id.btn_start_file_download)
     public void onClickStartDownload(){
-        downloadFile();
+        // for some reason it's not working for button click
+//        downloadFile();
     }
 
     public void downloadFile(){
@@ -56,7 +67,8 @@ public class DownloadActivity extends AppCompatActivity {
         call.enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                showMessage("Success!");
+                boolean success = writeResponseBodyToDisk(response.body());
+                showMessage("Download successful: " + success);
             }
 
             @Override
@@ -65,6 +77,55 @@ public class DownloadActivity extends AppCompatActivity {
             }
         });
     }
+
+    private boolean writeResponseBodyToDisk(ResponseBody body){
+        try {
+            File futureStudioFile = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                    "Future Studio Icon.png"
+            );
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try{
+                byte[] fileReader = new byte[4096];
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(futureStudioFile);
+
+                while(true){
+                    int read = inputStream.read(fileReader);
+                    if(read == -1){
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+                    fileSizeDownloaded += read;
+                    Log.d("Future Studio", "file upload: " + fileSizeDownloaded + " of " + fileSize);
+                }
+
+                outputStream.flush();
+                return true;
+            } catch (IOException e){
+                return false;
+            } finally {
+                if(inputStream != null){
+                    inputStream.close();
+                }
+
+                if(outputStream != null){
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e){
+            return false;
+        }
+    }
+
+    // TODO: override onRequestPermissionsResult()
 
     public void showMessage(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
